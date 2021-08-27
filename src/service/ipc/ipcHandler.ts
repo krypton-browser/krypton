af689ce833dd57ecfc56c5edc904e0ea48a52ec7
rmagur1203 <rmagur1203@naver.com>
2021-08-27 10:20:05 +0900

    ipcHandler bug fix

--- src/service/ipc/ipcHandler.ts
+++ src/service/ipc/ipcHandler.ts
@@ -2,6 +2,7 @@ import { ipcMain } from 'electron';
 
 export default function ipcHandler(channel: string): any {
   return (target: any, propertyKey: string) => {
-    if (target[propertyKey]) ipcMain?.on(channel, target[propertyKey]);
+    if (target[propertyKey] && typeof target[propertyKey] === 'function')
+      ipcMain?.on(channel, target[propertyKey]);
   };
 }
