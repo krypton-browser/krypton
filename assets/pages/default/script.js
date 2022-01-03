95ea05ef6614a400db6db0e4d86ac8dc8638b2cd
KHJcode <kimstudswcode4485@gmail.com>
2022-01-03 15:42:58 +0900

    default page bugfix

--- assets/pages/default/script.js
+++ assets/pages/default/script.js
@@ -2,10 +2,23 @@ const searchForm = document.querySelector('.search_form');
 const searchTextBox = document.querySelector('.search_text_box');
 const image = document.querySelector('.search_engine_image');
 
+function isUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === 'http:' || url.protocol === 'https:';
+}
+
 (function init() {
   searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    window.location.href = `${searchTextBox?.value}`;
+    const urlText = searchTextBox?.value;
+    window.location.href = isUrl(urlText)
+      ? urlText
+      : `https://duckduckgo.com/?q=${encodeURI(urlText)}`;
     searchTextBox.value = '';
   });
   const query = new URL(window.location.href).searchParams;
